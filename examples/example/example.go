9aca70ff3421b802de4673c81ecda3a433e61629
LiuYongGang <liuyonggangmail@sina.com>
2020-12-29 19:13:55 +0800

    feat(civilian):一些初始化

---
+++ examples/example/example.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GaryLiu666/civilian/pkg/utils"
+)
+
+func main() {
+	fmt.Println(utils.NewUuid())
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/GaryLiu666/civilian
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/google/uuid v1.1.2
+)
